fc40b11a0a8c23d0bcbccd9543a7a05a6731818e
Sidhant Pandey <sidhantpandeynp6@gmail.com>
2025-06-06 14:06:05 +0530

    * Add for Real time updates visibility in the mobile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'frontend',
-  webDir: '.next/server/app'
+  webDir: '.next/server/app',
+  server: {
+    url: 'http://192.168.2.102:3000', // your computer's IP, not localhost!
+    cleartext: true
+  }
 };
 
 export default config;
